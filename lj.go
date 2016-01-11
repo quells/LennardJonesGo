709992e4f0cff6f2cd36535f2916b88669489d1f
Kai Wells <support@kaiwells.me>
2016-01-11 10:43:41 -0800

    Cleaning up comments

--- lj.go
+++ lj.go
@@ -1,4 +1,4 @@
-// LennardJones simulates many bodied particle systems with the Lennard Jones potential.
+// LennardJones simulates molecular dynamics with the Lennard Jones potential.
 package main
 
 import (
@@ -12,7 +12,6 @@ type Globals struct {
 }
 
 func main() {
-	// fmt.Println("LennardJones v1.0.0")
 	g := Globals{
 		N: 64, L: 4.2323167,
 		T0: 0.728, M: 48.0,
@@ -23,10 +22,7 @@ func main() {
 	Vs := sim.InitVelocity(g.N, g.T0, g.M)
 
 	T := 1000
-	// fmt.Printf("0 %f\n", sim.TotalEnergy(Rs, Vs, g.L, g.M))
 	for t := 1; t < T; t++ {
 		Rs, Vs = sim.TimeStep(Rs, Vs, g.L, g.M, g.h)
-		// fmt.Printf("%d %f\n", t, sim.TotalEnergy(Rs, Vs, g.L, g.M))
-		// fmt.Printf("%d %f\n", t, sim.Temperature(Vs, g.M, g.N))
 	}
 }
