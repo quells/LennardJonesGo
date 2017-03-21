034581b37320fb16d8101b842e499fa1d5efef41
Kai Wells <support@kaiwells.me>
2017-03-21 10:34:48 -0700

    Update for system benchmarking

--- lj.go
+++ lj.go
@@ -2,7 +2,9 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
+	"time"
+
 	"github.com/quells/LennardJonesGo/sim"
 )
 
@@ -14,18 +16,19 @@ type Globals struct {
 
 func main() {
 	g := Globals{
-		N: 2048, L: 12.6992084,//6.3496042,//3.1748021,//4.2323167,
-		T0: 0.0, M: 48.0,
+		N: 2048, L: 12.6992084,
+		T0: 0.2, M: 48.0,
 		h: 0.01,
 	}
 
 	Rs := sim.InitPositionFCC(g.N, g.L)
 	Vs := sim.InitVelocity(g.N, g.T0, g.M)
 
-	T := 1000
+	T := 10
+	start := time.Now()
 	for t := 1; t <= T; t++ {
 		Rs, Vs = sim.TimeStepParallel(Rs, Vs, g.L, g.M, g.h)
-		//fmt.Println(sim.TotalEnergy(Rs, Vs, g.L, g.M))
-		//fmt.Println(sim.Temperature(Vs, g.M, g.N))
 	}
+	elapsed := time.Since(start)
+	fmt.Printf("%v for %d time steps\n", elapsed, T)
 }
